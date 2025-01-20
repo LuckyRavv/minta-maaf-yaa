eeb0804cab0b867d1254040a96ff169a4daf5690
LuckyRavv <raviazwa@gmail.com>
2025-01-20 17:25:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    const heart = document.getElementById('heart');
+    heart.classList.toggle('love-animation');
+});
