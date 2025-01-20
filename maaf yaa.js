bd190eb677b255fa90d1c5fadefb6d697cb976cf
LuckyRavv <raviazwa@gmail.com>
2025-01-20 17:25:27 +0700

    Delete maaf yaa.js

--- maaf yaa.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('animateButton').addEventListener('click', function() {
-    const heart = document.getElementById('heart');
-    heart.classList.toggle('love-animation');
-});
